95f4af762b88fe310a0a964bde1f34ac34f0c2b7
Prassad Raj R <prassadpr10@gmail.com>
2024-05-04 13:02:17 +0530

    chg

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,19 @@
 import Login from "./pages/Login/Login";
-
-
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Item from "./pages/items/Item";
 
 function App() {
-  return <div>
-    <Login/>
-  </div>;
+  return (
+    <div>
+      {/* <Login/> */}
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Item />} path="/item" />
+          <Route element={<Login/>} path="/login"/>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;

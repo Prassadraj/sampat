eeaff9fd2ed87ec3353edbfa89a284a5b7c26640
Prassad Raj R <prassadpr10@gmail.com>
2024-05-04 14:31:44 +0530

    chg

---
+++ client/src/assets/allProducts.js
@@ -0,0 +1,27 @@
+const allProduct = [
+  {
+    id: 1,
+    desc:"ronaldo",
+    img: "https://assets.manutd.com/AssetPicker/images/0/0/10/126/687707/Legends-Profile_Cristiano-Ronaldo1523460877263.jpg",
+  },
+  {
+    id: 2,
+    desc:"neymar",
+    img: "https://s.france24.com/media/display/ab532c1a-0778-11e9-8f28-005056bff430/w:1280/p:4x3/neymar-maillotpsg-m.jpg",
+  },
+  {
+    id: 3,
+    desc:"messi",
+    img: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQHnhWFBFpqpDEQE_DyEaYEXHwa8QY4mAsBTeZaif0XvmL1sXI2",
+  },
+  {
+    id: 4,
+    desc:"Ramous",
+    img: "https://i.eurosport.com/2023/09/04/3778333-76856588-2560-1440.jpg?w=1300",
+  },
+  {
+    id: 5,
+    desc:"MS Dhoni",
+    img: "https://static.tnn.in/thumb/msid-109671370,thumbsize-940574,width-1280,height-720,resizemode-75/109671370.jpg",
+  },
+];

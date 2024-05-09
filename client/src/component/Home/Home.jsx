ce4e026d1d36782b780c3000e8a416795364ca5e
Prassad Raj R <prassadpr10@gmail.com>
2024-05-09 16:32:50 +0530

    chg

--- client/src/component/Home/Home.jsx
+++ client/src/component/Home/Home.jsx
@@ -13,7 +13,7 @@ function Home() {
             Empowering Labs with Reliable Equipment
           </h1>
           <img
-            className="opacity-100 border-b-8 border-r-8 border-r-slate-600 border-b-slate-600 hover:shadow-2xl hover:border-r-green-600 hover:border-b-green-600 hover:opacity-95 duration-200"
+            className="opacity-100 border-b-8 border-r-8 border-r-slate-600 border-b-slate-600 hover:shadow-2xl hover:border-r-gray-600 hover:border-b-gray-600 hover:opacity-95 duration-200"
             width="90%"
             style={{ borderRadius: "10px" }}
             src="https://cpcdiagnostics.in/wp-content/uploads/2022/05/banner.jpg"

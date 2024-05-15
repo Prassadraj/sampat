492845cd3db43df540bba72a2783515e5ce1532c
Prassadraj <prassadpr10@gmail.com>
2024-05-15 17:58:22 +0530

    chg

--- client/src/component/Home/Home.jsx
+++ client/src/component/Home/Home.jsx
@@ -4,7 +4,7 @@ import Item from "../../pages/items/Item";
 import Contact from "../../pages/contact/Contact";
 import { Cursor } from "../Cursor";
 import "animate.css";
-import './home.css'
+import "./home.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFlaskVial } from "@fortawesome/free-solid-svg-icons";
 
@@ -14,9 +14,7 @@ function Home() {
       <Nav />
 
       <div className="flex flex-col justify-center mt-48 items-center gap-5">
-        <h1 className="text-3xl">
-          Empowering Labs with Reliable Equipment
-        </h1>
+        <h1 className="text-3xl">Empowering Labs with Reliable Equipment.</h1>
         <img
           className="w-auto opacity-100 border-b-8 border-r-8 border-r-slate-600 border-b-slate-600 hover:shadow-2xl hover:border-r-gray-600 hover:border-b-gray-600 hover:opacity-95 duration-200"
           style={{ borderRadius: "10px" }}

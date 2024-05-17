0f42996e8b11ab4b77c0691466867aff44087cce
Prassadraj <prassadpr10@gmail.com>
2024-05-17 13:06:20 +0530

    chg

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -8,7 +8,7 @@ function Home() {
     <div className="">
       <Nav />
 
-      <div className="flex flex-col justify-center mt-48 items-center gap-5">
+      <div className="flex flex-col justify-center mt-32 items-center gap-5">
         <h1 className="text-3xl">Empowering Labs with Reliable Equipment.</h1>
         <img
           className="w-auto opacity-100 border-b-8 border-r-8 border-r-slate-600 border-b-slate-600 hover:shadow-2xl hover:border-r-gray-600 hover:border-b-gray-600 hover:opacity-95 duration-200"

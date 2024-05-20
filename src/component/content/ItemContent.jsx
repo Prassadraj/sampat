e3e8d72e9e0f67f00cca303c9e2517a226808691
Prassadraj <prassadpr10@gmail.com>
2024-05-20 12:30:09 +0530

    chg

--- src/component/content/ItemContent.jsx
+++ src/component/content/ItemContent.jsx
@@ -1,9 +1,22 @@
 import React from "react";
+import { useInView } from "react-intersection-observer";
+import "./itemContent.css";
 
 function ItemContent() {
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+    threshold: 0.1,
+  });
+
   return (
-    <div className="flex justify-center  items-center p-7">
-      <h1 className=" text-3xl uppercase">
+    <div
+      className={`flex justify-center items-center p-7 ${
+        inView ? "animate-fade-in" : ""
+      }`}
+      ref={ref}
+      id="head"
+    >
+      <h1 className="text-3xl uppercase">
         Trending Now: Diagnostics Products Everyone's Talking About
       </h1>
     </div>
